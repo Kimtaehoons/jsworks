6a9eb4e92bdc6e6a2aa1fbca0b698fbf3ba74469
sharpboy77 <sharpboy77@naver.com>
2021-11-08 12:47:19 +0900

    day01

---
+++ day01/js/hello.js
@@ -0,0 +1,8 @@
+//웹 문서 본문 출력
+document.write("Hello~ world<br>");
+document.write("즐거운 웹 프로그래밍!!");
+//창 띄우기(객체(window)지향언어, alert는 함수)
+// window.alert("안녕하세요?")
+//alert("안녕하세요?")
+//window생략가능
+// script 주석 처리 - '//'한 줄 주석 처리, '/**/' 여러 줄 주석 처리
